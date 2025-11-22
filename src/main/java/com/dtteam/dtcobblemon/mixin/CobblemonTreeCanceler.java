801a2d34f6f236360962028dd092ce7a59887ead
Max Hyper <massimocantugarcia@gmail.com>
2025-11-22 20:17:17 -0300

    re-enabled tree cancellation

--- src/main/java/com/dtteam/dtcobblemon/mixin/CobblemonTreeCanceler.java
+++ src/main/java/com/dtteam/dtcobblemon/mixin/CobblemonTreeCanceler.java
@@ -16,12 +16,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CobblemonBiomeModifiers.class)
 public class CobblemonTreeCanceler {
-//    @Inject(method = "add", at = @At("HEAD"), cancellable = true, remap = false)
-//    private void cancelTreeGen(@NotNull ResourceKey<PlacedFeature> feature, @NotNull GenerationStep.Decoration step, @Nullable TagKey<Biome> validTag, CallbackInfo ci) {
-//        if(feature.isFor(Registries.PLACED_FEATURE) && GenerationStep.Decoration.VEGETAL_DECORATION.equals(step)) {
-//            if(feature.location().getPath().contains("tree")) {
-//                ci.cancel();
-//            }
-//        }
-//    }
+    @Inject(method = "add", at = @At("HEAD"), cancellable = true, remap = false)
+    private void cancelTreeGen(@NotNull ResourceKey<PlacedFeature> feature, @NotNull GenerationStep.Decoration step, @Nullable TagKey<Biome> validTag, CallbackInfo ci) {
+        if(feature.isFor(Registries.PLACED_FEATURE) && GenerationStep.Decoration.VEGETAL_DECORATION.equals(step)) {
+            if(feature.location().getPath().contains("tree")) {
+                ci.cancel();
+            }
+        }
+    }
 }
