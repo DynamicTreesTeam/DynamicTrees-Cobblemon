056a9b1500e37769de70f977c77f731aff384ff5
Max Hyper <massimocantugarcia@gmail.com>
2025-11-23 03:12:24 -0300

    added saccharine cone texture

--- src/main/java/com/dtteam/dtcobblemon/init/DTCobblemonPoiTypes.java
+++ src/main/java/com/dtteam/dtcobblemon/init/DTCobblemonPoiTypes.java
@@ -1,6 +1,7 @@
 package com.dtteam.dtcobblemon.init;
 
 import com.dtteam.dtcobblemon.DynamicTreesCobblemon;
+import com.dtteam.dtcobblemon.tree.SaccharineFamily;
 import com.dtteam.dynamictrees.tree.family.Family;
 import com.google.common.collect.ImmutableSet;
 import net.minecraft.core.registries.Registries;
@@ -18,10 +19,10 @@ public class DTCobblemonPoiTypes {
     public static DeferredHolder<PoiType, PoiType> SACCHARINE_BRANCH_SLATHERED;
 
     public static void register(IEventBus bus) {
-        Family.REGISTRY.runOnNextLock(Family.REGISTRY.generateIfValidRunnable(DynamicTreesCobblemon.location("saccharine"), saccharine -> {
-            if (saccharine.isValid()){
+        Family.REGISTRY.runOnNextLock(Family.REGISTRY.generateIfValidRunnable(DynamicTreesCobblemon.location("saccharine"), family -> {
+            if (family.isValid() && family instanceof SaccharineFamily saccharine){
                 SACCHARINE_BRANCH_SLATHERED = POIs_REGISTRY.register("saccharine_branch_slathered", () ->
-                        new PoiType(ImmutableSet.copyOf(saccharine.getBranch().get().getStateDefinition().getPossibleStates()), 0, 1)
+                        new PoiType(ImmutableSet.copyOf(saccharine.getSlatheredBranch().get().getStateDefinition().getPossibleStates()), 0, 1)
                 );
                 POIs_REGISTRY.register(bus);
             }
--- src/main/java/com/dtteam/dtcobblemon/mixin/SaccharineSlatheredInfluence.java
+++ src/main/java/com/dtteam/dtcobblemon/mixin/SaccharineSlatheredInfluence.java
@@ -6,18 +6,22 @@ import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
 import com.cobblemon.mod.common.net.messages.client.effect.SaccharineLogBlockParticlesPacket;
 import com.dtteam.dtcobblemon.branch.SlatheredSaccharineBranchBlock;
 import com.dtteam.dynamictrees.tree.TreeHelper;
+import kotlin.jvm.functions.Function1;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -25,8 +29,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(SaccharineLogSlatheredInfluence.class)
 public class SaccharineSlatheredInfluence {
 
-    @Shadow
-    private boolean activated;
+    @Unique
+    private boolean dynamicTrees_Cobblemon_1_21_1$activated;
 
     @Final @Shadow
     private BlockPos pos;
@@ -36,7 +40,7 @@ public class SaccharineSlatheredInfluence {
 
     @Inject(method = "affectSpawn", at = @At("TAIL"), remap = false)
     private void affectSpawn(SpawnAction<?> action, Entity entity, CallbackInfo ci){
-        if (entity instanceof PokemonEntity pokemonEntity && !activated) {
+        if (entity instanceof PokemonEntity pokemonEntity && !dynamicTrees_Cobblemon_1_21_1$activated) {
             BlockPos logPos = this.pos;
             ServerLevel level = action.getSpawnablePosition().getWorld().getLevel();
 
@@ -53,7 +57,12 @@ public class SaccharineSlatheredInfluence {
                     }
 
                     new SaccharineLogBlockParticlesPacket(logPos, safePos)
-                            .sendToPlayersAround(logPos.getX(), logPos.getY(), logPos.getZ(), 64.0, level.dimension(), null);
+                            .sendToPlayersAround(logPos.getX(), logPos.getY(), logPos.getZ(), 64.0, level.dimension(), new Function1<ServerPlayer, Boolean>() {
+                                @Override
+                                public Boolean invoke(ServerPlayer serverPlayer) {
+                                    return false;
+                                }
+                            });
 
                     level.playSound(null, logPos, SoundEvents.PLAYER_BURP, SoundSource.NEUTRAL);
 
@@ -61,10 +70,11 @@ public class SaccharineSlatheredInfluence {
                     if (radius > 0)
                         branch.getFamily().getBranch().ifPresent(b -> b.setRadius(level, logPos, radius, Direction.DOWN));
                 }
+//                else {
+//                    level.setBlock(logPos, Blocks.GOLD_BLOCK.defaultBlockState(), 3);
+//                }
             }
-
-            pokemonEntity.getPokemon().getForcedAspects().add(SaccharineLogSlatheredInfluence.HONEY_DRENCHED_ASPECT);
-            activated = true;
+            dynamicTrees_Cobblemon_1_21_1$activated = true;
         }
     }
 
