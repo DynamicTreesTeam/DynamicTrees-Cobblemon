88e7bb436a7a2d6ec66c867c3a3f89ab9d25f347
Max Hyper <massimocantugarcia@gmail.com>
2025-11-22 21:56:22 -0300

    added beenest placement to saccharine trees

--- src/main/java/com/dtteam/dtcobblemon/DTCobblemonRegistries.java
+++ src/main/java/com/dtteam/dtcobblemon/DTCobblemonRegistries.java
@@ -3,17 +3,27 @@ package com.dtteam.dtcobblemon;
 import com.dtteam.dtcobblemon.fruit.ApricornFruit;
 import com.dtteam.dtcobblemon.tree.SaccharineFamily;
 import com.dtteam.dtcobblemon.tree.SaccharineSpecies;
+import com.dtteam.dynamictrees.api.registry.Registry;
 import com.dtteam.dynamictrees.block.fruit.Fruit;
+import com.dtteam.dynamictrees.data.tags.DTBiomeTags;
 import com.dtteam.dynamictrees.event.ApplierRegistryEvent;
+import com.dtteam.dynamictrees.event.RegistryEvent;
 import com.dtteam.dynamictrees.event.TypeRegistryEvent;
+import com.dtteam.dynamictrees.systems.genfeature.BeeNestGenFeature;
+import com.dtteam.dynamictrees.systems.genfeature.GenFeature;
+import com.dtteam.dynamictrees.systems.genfeature.GenFeatureConfiguration;
 import com.dtteam.dynamictrees.systems.genfeature.GenFeatures;
 import com.dtteam.dynamictrees.tree.family.Family;
 import com.dtteam.dynamictrees.tree.species.Species;
 import com.google.gson.JsonElement;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.Blocks;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import org.jetbrains.annotations.NotNull;
 
 import static com.dtteam.dynamictrees.systems.genfeature.BeeNestGenFeature.WORLD_GEN_CHANCE_FUNCTION;
 import static com.dtteam.dynamictrees.systems.genfeature.GenFeature.CAN_GROW_PREDICATE;
@@ -41,5 +51,34 @@ public class DTCobblemonRegistries {
             event.registerType(DynamicTreesCobblemon.Location("saccharine"), SaccharineFamily.TYPE);
         }
     }
+
+    @SubscribeEvent
+    public static void onGenFeatureRegistry(final RegistryEvent<GenFeature> event) {
+        if (!event.isEntryOfType(GenFeature.class)) return;
+        event.getRegistry().register(new BeeNestGenFeature(DynamicTreesCobblemon.Location("saccharine_bee_nest")){
+            @Override @NotNull
+            public GenFeatureConfiguration createDefaultConfiguration() {
+                return super.createDefaultConfiguration()
+                        .with(NEST_BLOCK, Blocks.BEE_NEST)
+                        .with(MAX_HEIGHT, 32)
+                        .with(CAN_GROW_PREDICATE, (world, pos) -> {
+                            if (world.getRandom().nextFloat() > SaccharineSpecies.BeeNestGrowChance) {
+                                return false;
+                            }
+                            // Default flower check predicate, straight from AbstractTreeGrower
+                            for (BlockPos blockpos : BlockPos.betweenClosed(pos.below().north(2).west(2), pos.above().south(2).east(2))) {
+                                if (world.getBlockState(blockpos).is(BlockTags.FLOWERS)) {
+                                    return true;
+                                }
+                            }
+                            return false;
+                        })
+                        .with(WORLD_GEN_CHANCE_FUNCTION, (world, pos) -> SaccharineSpecies.BeeNestWorldGenChance)
+                        .with(MAX_COUNT, 1);
+            }
+        });
+    }
+
+
 
 }
